c443c8951c8d6a09af3f73fe7196d622b9576362
tgalpha <tanalpha.zhy@gmail.com>
2024-03-19 11:51:52 +0800

    patch android build script

--- src/wpe/wp_patch/build.py
+++ src/wpe/wp_patch/build.py
@@ -21,6 +21,9 @@ import sys
 import re
 from common.constant import PLUGIN_NAME, PROJECT_ROOT
 from common.platform import *
+# [wp-enhanced patch] import android platform patch
+from wpe.wp_patch.common.platform import android
+# [/wp-enhanced patch]
 from common.registry import platform_registry, get_supported_platforms
 from common.util import exit_with_error
 
