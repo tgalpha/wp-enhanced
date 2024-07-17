55b17bef14d7586d0015f66462ea3f7e0ed477bd
tgalpha <tanalpha.zhy@gmail.com>
2024-07-17 11:36:01 +0800

    update pre_premake hook template

--- src/wpe/templates/.wpe/hooks/pre_premake.py
+++ src/wpe/templates/.wpe/hooks/pre_premake.py
@@ -2,13 +2,15 @@ import subprocess
 import os.path as osp
 
 
-def fetch_git_repo(url, dst_dir, branch='master'):
-    if osp.exists(dst_dir):
+def fetch_git_repo(url, dst_dir, branch='master', keep_to_latest=True):
+    if not osp.exists(dst_dir):
+        print(f'Cloning {url} to {dst_dir}')
+        subprocess.check_call(['git', 'clone', '-b', branch, url, dst_dir])
+        return
+    if keep_to_latest:
         print(f'Updating {dst_dir}')
         subprocess.check_call(['git', 'pull'], cwd=dst_dir)
         return
-    print(f'Cloning {url} to {dst_dir}')
-    subprocess.check_call(['git', 'clone', '-b', branch, url, dst_dir])
 
 
 def main(**kwargs):
